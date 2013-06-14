ea9d9828fc5f93f09dcf101b676822809ff4461c
Matthew Tretter <m@tthewwithanm.com>
2013-06-14 16:56:15 -0400

    Bump version to 1.0b1

--- throttleandcache/pkgmeta.py
+++ throttleandcache/pkgmeta.py
@@ -1,5 +1,5 @@
 __title__ = 'django-throttleandcache'
 __author__ = 'Matthew Tretter'
-__version__ = '0.1.0'
+__version__ = '1.0b1'
 __license__ = 'BSD'
 __all__ = ['__title__', '__author__', '__version__', '__license__']
