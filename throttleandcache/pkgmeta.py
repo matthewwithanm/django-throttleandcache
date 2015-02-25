b97bc1a9dd4a0960331c55d3897d0c39f9b78603
Matthew Dapena-Tretter <m@tthewwithanm.com>
2015-02-25 10:55:52 -0500

    Bump to 2.1.0

--- throttleandcache/pkgmeta.py
+++ throttleandcache/pkgmeta.py
@@ -1,5 +1,5 @@
 __title__ = 'django-throttleandcache'
 __author__ = 'Matthew Dapena-Tretter'
-__version__ = '2.0.2'
+__version__ = '2.1.0'
 __license__ = 'BSD'
 __all__ = ['__title__', '__author__', '__version__', '__license__']
