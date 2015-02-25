d6264da1fdeda38132a77ce3b31d449a35f3b72b
Matthew Dapena-Tretter <m@tthewwithanm.com>
2015-02-25 10:33:05 -0500

    :lipstick:

--- throttleandcache/conf.py
+++ throttleandcache/conf.py
@@ -2,30 +2,37 @@ from appconf import AppConf
 from django.conf import settings
 
 
+def get_dummy_cache():
+    try:
+        from django.core.cache.backends.dummy import DummyCache  # noqa
+    except ImportError:
+        return 'dummy://'
+    else:
+        return 'django.core.cache.backends.dummy.DummyCache'
+
+
+def get_default_cache_alias():
+    # DEFAULT_CACHE_ALIAS doesn't exist in Django<=1.2
+    try:
+        from django.core.cache import DEFAULT_CACHE_ALIAS
+        return DEFAULT_CACHE_ALIAS
+    except ImportError:
+        return 'default'
+
+
 class ThrottleAndCacheConf(AppConf):
     DEFAULT_CACHE = None
     MAX_TIMEOUT = 60 * 60 * 24 * 365 * 10
 
     def configure_default_cache(self, value):
-        if value is None:
-            try:
-                from django.core.cache.backends.dummy import DummyCache
-            except ImportError:
-                dummy_cache = 'dummy://'
-            else:
-                dummy_cache = 'django.core.cache.backends.dummy.DummyCache'
+        if value is not None:
+            return value
 
-            # DEFAULT_CACHE_ALIAS doesn't exist in Django<=1.2
-            try:
-                from django.core.cache import DEFAULT_CACHE_ALIAS as default_cache_alias
-            except ImportError:
-                default_cache_alias = 'default'
+        if settings.DEBUG:
+            return get_dummy_cache()
 
-            if settings.DEBUG:
-                value = dummy_cache
-            elif default_cache_alias in getattr(settings, 'CACHES', {}):
-                value = default_cache_alias
-            else:
-                value = getattr(settings, 'CACHE_BACKEND', None) or dummy_cache
+        default_cache_alias = get_default_cache_alias()
+        if default_cache_alias in getattr(settings, 'CACHES', {}):
+            return default_cache_alias
 
-        return value
+        return getattr(settings, 'CACHE_BACKEND', None) or get_dummy_cache()
